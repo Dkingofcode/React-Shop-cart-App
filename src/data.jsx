166c0a48df7aa0c39ee002cc9dca3ab4ad7530e2
Dkingofcode <doladepo128@gmail.com>
2023-04-04 11:56:10 +0100

    First commit

---
+++ src/data.jsx
@@ -0,0 +1,44 @@
+const cartItems = [
+  {
+    id: 'rec1JZlfCIBM0tyERT',
+    title: 'Samsung Glaxy S8',
+    price: '399.99',
+    img: 'https://images2.imgbox.com/c2/14/zedmXgs6_o._png',
+    amount: 1,
+  },
+  {
+    id: 'rec1JZlfCIBM0tyERT',
+    title: 'Samsung Glaxy S8',
+    price: '399.99',
+    img: 'matteo-vella-mJif93InGaU-unsplash.jpg',
+    amount: 1,
+  },
+  {
+    id: 'rec1JZlfCIBM0tyERT',
+    title: 'Iphone Xr',
+    price: '399.99',
+    img: 'rishabh-malhotra-83ypHTv6J2M-unsplash.jpg',
+    amount: 1,
+  },
+  {
+    id: 'rec1JZlfCIBM0tyERT',
+    title: 'Iphone XS max',
+    price: '399.99',
+    img: 'shiwa-id-F_cfF1uX7qY-unsplash.jpg',
+    amount: 1,
+  },
+  {
+    id: 'rec1JZlfCIBM0tyERT',
+    title: 'Iphone X',
+    price: '399.99',
+    img: 'thom-bradley-1NZcjdo2hKQ-unsplash.jpg',
+    amount: 1,
+  },
+  {
+    id: 'rec1JZlfCIBM0tyERT',
+    title: ' Google pixel',
+    price: '399.99',
+    img: 'https://images2.imgbox.com/c2/14/zedmXgs6_o._png',
+    amount: 1,
+  },
+]
